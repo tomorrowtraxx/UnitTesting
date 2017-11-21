67352e74927ac9528aef388ea42ebc7935f003f6
tomorrowtraxx <tomorrowtraxx@gmail.com>
2017-11-21 19:16:44 +0100

    Include Fixture

---
+++ Labs/Lab04/Starter/Implementation.UnitTest/Fixtures/ShoppingCartFixture.cs
@@ -0,0 +1,159 @@
+﻿using Implementation.Repository;
+using Implementation.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace Implementation.UnitTest.Fixtures
+{
+    public class ShoppingCartFixture
+    {
+        private ShoppingCart _cart;
+        private User _user;
+
+        //private Mock<IUserRepository> _userRepository;
+        //private Mock<IBankingService> _bankingService;
+
+        public ShoppingCartFixture()
+        {
+            //_userRepository = new Mock<IUserRepository>();
+            //_bankingService = new Mock<IBankingService>();
+
+            _shoppingCartConfiguration = new ShoppingCartBuilder();
+            _userConfiguration = new UserBuilder();
+        }
+
+        private ShoppingCartBuilder _shoppingCartConfiguration;
+        public ShoppingCartBuilder ShoppingCartConfiguration
+        {
+            get
+            {
+                return _shoppingCartConfiguration;
+            }
+        }
+
+        private UserBuilder _userConfiguration;
+        public UserBuilder UserConfiguration
+        {
+            get
+            {
+                return _userConfiguration;
+            }
+        }
+
+        public void Setup()
+        {
+            _cart = ShoppingCartConfiguration.Build();
+            _user = UserConfiguration.Build();
+            _shoppingCartConfiguration.UserRepository.Setup(x => x.GetUser(It.IsAny<string>())).Returns(_user);
+        }
+
+        public void CheckoutCart()
+        {
+            _cart.CheckOut();
+        }
+
+        public void AddProductToCart(Product product, int amount)
+        {
+            _cart.Add(product, amount);
+        }
+
+        public void AssertProductIsInCart(Product expectedItem, int expectedAmount)
+        {
+            Assert.IsTrue(_cart.Orders.ContainsKey(expectedItem));
+            Assert.AreEqual(expectedAmount, _cart.Orders[expectedItem]);
+        }
+
+        public void VerifyBalanceFetched()
+        {
+            _shoppingCartConfiguration.BankingService.Verify(x => x.GetBalance(_user.AccountNumber));
+        }
+
+        public void VerifyUserFetched()
+        {
+            _shoppingCartConfiguration.UserRepository.Verify(x => x.GetUser(_cart.Owner));
+        }
+
+        public void AssertCartTotalIsCorrect(double amount)
+        {
+            Assert.AreEqual(amount, _cart.Total);
+        }
+
+        public class ShoppingCartBuilder
+        {
+            private string _userName;
+            private Mock<IUserRepository> _userRepository;
+            private Mock<IBankingService> _bankingService;
+
+            public Mock<IUserRepository> UserRepository { get => _userRepository; }
+            public Mock<IBankingService> BankingService { get => _bankingService; }
+
+            public ShoppingCartBuilder()
+            {
+                _userName = "Jasper";
+                _userRepository = new Mock<IUserRepository>();
+                _bankingService = new Mock<IBankingService>();
+            }
+
+            public ShoppingCartBuilder WithUserName(string userName)
+            {
+                _userName = userName;
+                return this;
+            }
+
+            //public ShoppingCartBuilder WithUserRepository(IUserRepository userRepository)
+            //{
+            //    _userRepository = userRepository;
+            //    return this;
+            //}
+
+            //public ShoppingCartBuilder WithBankingService(IBankingService bankingService)
+            //{
+            //    _bankingService = bankingService;
+            //    return this;
+            //}
+
+            public ShoppingCart Build()
+            {
+                return new ShoppingCart(_userName, _userRepository.Object, _bankingService.Object);
+            }
+        }
+
+        public class UserBuilder
+        {
+            private string _name;
+            private string _accountNumber;
+            private DateTime _dateOfBirth;
+
+            public UserBuilder()
+            {
+                _name = "Jasper";
+                _accountNumber = "0000000000000";
+                _dateOfBirth = new DateTime(1900, 1, 1);
+            }
+
+            public UserBuilder WithAccountNumber(string accountNumber)
+            {
+                _accountNumber = accountNumber;
+                return this;
+            }
+
+            public UserBuilder WithDateOfBirth(DateTime dateOfBirth)
+            {
+                _dateOfBirth = dateOfBirth;
+                return this;
+            }
+
+            public UserBuilder WithName(string name)
+            {
+                _name = name;
+                return this;
+            }
+
+            public User Build()
+            {
+                return new User(_name, _dateOfBirth, _accountNumber);
+            }
+        }
+    }
+}
--- Labs/Lab04/Starter/Implementation.UnitTest/ShoppingCartTest.cs
+++ Labs/Lab04/Starter/Implementation.UnitTest/ShoppingCartTest.cs
@@ -1,6 +1,7 @@
 ﻿using Implementation.Repository;
 using Implementation.Service;
 using Implementation.UnitTest.Builders;
+using Implementation.UnitTest.Fixtures;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
@@ -10,10 +11,10 @@ namespace Implementation.UnitTest
     [TestClass]
     public class ShoppingCartTest
     {
-        private Mock<IUserRepository> _userRepository;
-        private Mock<IBankingService> _bankingService;
+        //private Mock<IUserRepository> _userRepository;
+        //private Mock<IBankingService> _bankingService;
 
-        private ShoppingCart _cart;
+        //private ShoppingCart _cart;
 
         private Product Xbox = new Product("Xbox 360", 199.99);
         private Product Playstation = new Product("PlayStation3", 250);
@@ -21,126 +22,116 @@ namespace Implementation.UnitTest
         [TestInitialize]
         public void TestInitialize()
         {
-            _userRepository = new Mock<IUserRepository>();
-            _bankingService = new Mock<IBankingService>();
+            //_userRepository = new Mock<IUserRepository>();
+            //_bankingService = new Mock<IBankingService>();
         }
 
         [TestMethod]
         public void Cart_Should_Contain_Product_After_Product_Is_Added()
         {
-            _cart = new ShoppingCartBuilder()
-                .WithUserRepository(_userRepository.Object)
-                .WithBankingService(_bankingService.Object)
-                .Build();
+            var fixture = new ShoppingCartFixture();
 
-            _cart.Add(Xbox, 2);
-            AssertProductIsInCart(Xbox, 2);
+            fixture.Setup();
+
+            fixture.AddProductToCart(Xbox, 2);
+
+            fixture.AssertProductIsInCart(Xbox, 2);
         }
 
         [TestMethod]
         public void Cart_Should_Update_Amount_When_Same_Product_Is_Added_Twice()
         {
-            _cart = new ShoppingCartBuilder()
-                .WithUserRepository(_userRepository.Object)
-                .WithBankingService(_bankingService.Object)
-                .Build();
+            var fixture = new ShoppingCartFixture();
 
-            _cart.Add(Xbox, 2);
-            _cart.Add(Xbox, 3);
+            fixture.Setup();
 
-            AssertProductIsInCart(Xbox, 5);
+            fixture.AddProductToCart(Xbox, 2);
+            fixture.AddProductToCart(Xbox, 3);
+
+            fixture.AssertProductIsInCart(Xbox, 5);
         }
 
         [TestMethod]
         public void Cart_should_Contain_Different_Products_After_Products_Are_Added()
         {
-            _cart = new ShoppingCartBuilder()
-                .WithUserRepository(_userRepository.Object)
-                .WithBankingService(_bankingService.Object)
-                .Build();
+            var fixture = new ShoppingCartFixture();
 
-            _cart.Add(Xbox, 1);
-            _cart.Add(Playstation, 2);
+            fixture.Setup();
 
-            AssertProductIsInCart(Xbox, 1);
-            AssertProductIsInCart(Playstation, 2);
+            fixture.AddProductToCart(Xbox, 1);
+            fixture.AddProductToCart(Playstation, 2);
+
+            fixture.AssertProductIsInCart(Xbox, 1);
+            fixture.AssertProductIsInCart(Playstation, 2);
         }
 
         [TestMethod]
         public void Empty_Cart_Total_Should_Be_Zero()
         {
-            _cart = new ShoppingCartBuilder()
-                .WithUserRepository(_userRepository.Object)
-                .WithBankingService(_bankingService.Object)
-                .Build();
+            var fixture = new ShoppingCartFixture();
 
-            Assert.AreEqual(0, _cart.Total);
+            fixture.Setup();
+
+            //Assert.AreEqual(0, _cart.Total);
+            fixture.AssertCartTotalIsCorrect(0);
         }
 
         [TestMethod]
         public void Carts_Total_Should_Be_Sum_Of_Products_Price_Times_Amount()
         {
-            _cart = new ShoppingCartBuilder()
-                .WithUserRepository(_userRepository.Object)
-                .WithBankingService(_bankingService.Object)
-                .Build();
+            var fixture = new ShoppingCartFixture();
 
-            _cart.Add(Playstation, 2); //500
-            _cart.Add(Xbox, 1); //199.99
+            fixture.Setup();
 
-            Assert.AreEqual(699.99, _cart.Total);
+            fixture.AddProductToCart(Playstation, 2); //500
+            fixture.AddProductToCart(Xbox, 1); //199.99
+
+            //Assert.AreEqual(699.99, _cart.Total);
+            fixture.AssertCartTotalIsCorrect(699.99);
         }
 
         [TestMethod]
         public void User_Should_Be_Fetched_From_Repository_On_Checkout()
         {
-            User frank = new UserBuilder()
+            var fixture = new ShoppingCartFixture();
+            fixture.UserConfiguration
                 .WithName("Frank")
                 .WithDateOfBirth(new DateTime(1978, 9, 27))
-                .WithAccountNumber("1234-84")
-                .Build();
+                .WithAccountNumber("1234-84");
+            fixture.ShoppingCartConfiguration
+                .WithUserName("Frank");
 
-            _cart = new ShoppingCartBuilder()
-                .WithUserRepository(_userRepository.Object)
-                .WithBankingService(_bankingService.Object)
-                .WithUserName("Frank")
-                .Build();
+            fixture.Setup();
 
-            _userRepository.Setup(x => x.GetUser(It.IsAny<string>())).Returns(frank);
+            fixture.AddProductToCart(Xbox, 1);
+            fixture.CheckoutCart();
 
-            _cart.Add(Xbox, 1);
-            _cart.CheckOut();
-
-            _userRepository.Verify(x => x.GetUser(_cart.Owner));
+            fixture.VerifyUserFetched();
         }
 
         [TestMethod]
         public void Balance_Should_Be_Fetched_From_BankingService_On_Checkout()
         {
-            User frank = new UserBuilder()
+            var fixture = new ShoppingCartFixture();
+            fixture.UserConfiguration
                 .WithName("Frank")
                 .WithDateOfBirth(new DateTime(1978, 9, 27))
-                .WithAccountNumber("1234-84")
-                .Build();
+                .WithAccountNumber("1234-84");
+            fixture.ShoppingCartConfiguration
+                .WithUserName("Frank");
 
-            _cart = new ShoppingCartBuilder()
-                .WithUserRepository(_userRepository.Object)
-                .WithBankingService(_bankingService.Object)
-                .WithUserName("Frank")
-                .Build();
+            fixture.Setup();
 
-            _userRepository.Setup(x => x.GetUser(It.IsAny<string>())).Returns(frank);
+            fixture.AddProductToCart(Xbox, 1);
+            fixture.CheckoutCart();
 
-            _cart.Add(Xbox, 1);
-            _cart.CheckOut();
-
-            _bankingService.Verify(x => x.GetBalance(frank.AccountNumber));
+            fixture.VerifyBalanceFetched();
         }
 
-        private void AssertProductIsInCart(Product expectedItem, int expectedAmount)
-        {
-            Assert.IsTrue(_cart.Orders.ContainsKey(expectedItem));
-            Assert.AreEqual(expectedAmount, _cart.Orders[expectedItem]);
-        }
+        //private void AssertProductIsInCart(Product expectedItem, int expectedAmount)
+        //{
+        //    Assert.IsTrue(_cart.Orders.ContainsKey(expectedItem));
+        //    Assert.AreEqual(expectedAmount, _cart.Orders[expectedItem]);
+        //}
     }
 }
